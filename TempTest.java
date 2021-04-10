5a12a223ca8e49e490352821fb0d7b6d83c670e8
ivasilopoulos <82248777+ivasilopoulos@users.noreply.github.com>
2021-04-10 06:15:26 +0300

    Add files via upload
    Project Plan + Code

---
+++ TempTest.java
@@ -0,0 +1,18 @@
+
+package temptest;
+
+
+/**
+ *
+ * @author ivasilop
+ */
+public class TempTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      System.out.println("This is just the beginning...");
+    }
+    
+}
