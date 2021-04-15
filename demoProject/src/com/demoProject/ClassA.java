9fa507cf0e3f910836895651e2c4a15c836dbb8e
Koios Visual Programming Tool <koios.vpt@gmail.com>
2021-04-15 14:23:36 +0300

    ClassB creation

--- demoProject/src/com/demoProject/ClassA.java
+++ demoProject/src/com/demoProject/ClassA.java
@@ -3,14 +3,11 @@ package com.demoProject;
 import javax.swing.*;
 
 public class ClassA {
-    String msg="";
-
-    public ClassA()
-    {this.msg=msg;}
 
     public void showMsg()
     {
         JFrame f=new JFrame();
         JOptionPane.showMessageDialog(f,"This is a message from ClassA");
+        f.dispose();
     }
 }
--- demoProject/src/com/demoProject/ClassB.java
+++ demoProject/src/com/demoProject/ClassB.java
@@ -1,4 +1,14 @@
 package com.demoProject;
 
+import javax.swing.*;
+
 public class ClassB extends ClassA{
+
+   /* public void showMsg()
+    {
+        JFrame f=new JFrame();
+        JOptionPane.showMessageDialog(f,"This is a message from ClassB");
+        f.dispose
+    }*/
 }
+
