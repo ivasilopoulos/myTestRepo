760d8afa9fc75965eaaf748b1e8911f84b1c86cb
ivasilopoulos <82248777+ivasilopoulos@users.noreply.github.com>
2021-04-15 15:30:56 +0300

    Update ClassB.java

--- demoProject/src/com/demoProject/ClassB.java
+++ demoProject/src/com/demoProject/ClassB.java
@@ -7,7 +7,7 @@ public class ClassB extends ClassA{
    /* public void showMsg()
     {
         JFrame f=new JFrame();
-        JOptionPane.showMessageDialog(f,"This is a message from ClassB!");
+        JOptionPane.showMessageDialog(f,"This is a message from ClassB!!!!");
         f.dispose
     }*/
 }
