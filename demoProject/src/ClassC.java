8685a83565541273193908e92c413c374f8fdcce
ivasilopoulos <82248777+ivasilopoulos@users.noreply.github.com>
2022-05-24 09:31:26 +0300

    Create ClassC.java
    New class for new functionality

---
+++ demoProject/src/ClassC.java
@@ -0,0 +1,13 @@
+package com.demoProject;
+
+import javax.swing.*;
+
+public class ClassC extends ClassA{
+
+    public void showMsg()
+    {
+        JFrame f=new JFrame();
+        JOptionPane.showMessageDialog(f,"This is a message from ClassC!!!!!!");
+        f.dispose();
+    }
+}
